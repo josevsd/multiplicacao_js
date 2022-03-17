3e6d8fa2c6480f9cca4129735088f5a570493b3b
josevsd <88160424+josevsd@users.noreply.github.com>
2022-03-16 22:22:02 -0300

    commit init

---
+++ index.js
@@ -0,0 +1,14 @@
+function buscar(){
+    let valor = document.getElementById('valor').value
+    let resposta = document.getElementById('resposta')
+
+    for(let i = 0; i <11; i++){
+        var soma 
+
+        soma = valor * i 
+
+        console.log(soma)
+    }
+
+
+}
